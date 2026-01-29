5429e62e2d80c9784047b37e6cf34323eda99cf5
Daniel Opara <opara88daniel@gmail.com>
2026-01-28 21:09:46 -0500

    update file structure

--- APCSProject/MainGameNec/battleObject.java
+++
@@ -1,7 +0,0 @@
-package MainGameNec;
-public interface battleObject<T> {
-    public void attack(T target) throws InterruptedException;
-    public void defend() throws InterruptedException;
-    public void special(statClass target) throws InterruptedException;
-    public boolean isDead() throws InterruptedException;
-}
